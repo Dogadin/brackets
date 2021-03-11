0d3fb2f0ced0b091c37fd704500fd6ae401ea7e5
Dogadin <AlexanderDogadin.7.0@gmail.com>
2021-03-11 18:07:06 +0300

    feat: index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let oldStr = ''
+
+  let found = true
+
+  do{
+    oldStr = str
+
+    bracketsConfig.forEach(item => {
+      const repl = item[0] + item[1]
+      str = str.replace(repl,'')
+    });
+
+    if(str==oldStr){
+      found = false
+    } else {
+      found = true
+    }
+    if(str.length==0){found= false}
+  } while (found)
+
+  if (str.length == 0){return true}
+  return false
 }
